185ac453838bfa0e9268b56970833adb2f72de88
Manas Acharyya <manas.acharyya.nits.cse18@gmail.com>
2021-04-12 15:15:48 +0530

    CSS

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+import './index.css'
+
 // Create a Function that returns JSX( HTML of React World).
 // Function Name must start with CAPS 
-function Greeting() {
-  return <h1>Hello World!</h1>
+function Banner() {
+  return (
+    <React.Fragment>
+      <h1>
+        Manas Pratim Acharyya
+      </h1>
+      <section>
+        <article>manas.acharyya.nits.cse18@gmail.com</article>
+        <article>+91 7896 227982</article>
+        <section>linkedin.com/in/manasacharyya25</section>
+        <section>github.com/manasacharyya25</section>
+      </section>
+    </React.Fragment>
+  )
 }
 
 //Inject the JSX returned by above function, into Index.html
-ReactDom.render(<Greeting />, document.getElementById('root'))
+ReactDom.render(<Banner />, document.getElementById('root'))
