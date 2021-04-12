c2ef83901a5f186d9b884bbdd01907c3768cf3b0
Manas Acharyya <manas.acharyya.nits.cse18@gmail.com>
2021-04-12 17:12:52 +0530

    Added Banner

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -7,17 +7,43 @@ import './index.css'
 // Function Name must start with CAPS 
 function Banner() {
   return (
-    <React.Fragment>
-      <h1>
-        Manas Pratim Acharyya
-      </h1>
-      <section>
-        <article>manas.acharyya.nits.cse18@gmail.com</article>
-        <article>+91 7896 227982</article>
-        <section>linkedin.com/in/manasacharyya25</section>
-        <section>github.com/manasacharyya25</section>
+    <section className='banner'>
+      <span className="banner-head">
+        <div className='banner-image'>
+          <img src='https://media-exp1.licdn.com/dms/image/C5103AQHifPVvdvOtxA/profile-displayphoto-shrink_400_400/0/1558908973129?e=1623888000&v=beta&t=ou9_e0IUFeUAk7zKOBT-CsJNBtq5LIE4F03v-kVRA4o' ></img>
+        </div>
+        <div className="banner-title">
+          <h1>Manas Pratim Acharyya</h1>
+        </div>
+      </span>
+      <section className='banner-links'>
+        <Links
+          link='manas.acharyya.nits.cse18@gmail.com'
+          img='./assets/gmail.png' />
+
+        <Links link='+91 7896 227982' img='./assets/contact.png' />
+
+        <Links
+          link='linkedin.com/in/manasacharyya25'
+          img='./assets/linkedin.png' />
+
+        <Links
+          link='github.com/manasacharyya25'
+          img='/assets/github.png' />
+
+        <Links />
       </section>
-    </React.Fragment>
+      <div className='separator'></div>
+    </section>
+  )
+}
+
+const Links = ({ link, img }) => {
+  return (
+    <div className='links'>
+      <img src={img} />
+      <a href={link}>{link}</a>
+    </div>
   )
 }
 
