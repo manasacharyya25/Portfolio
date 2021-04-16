2a118d19b5bc749a76f04c7d8e830d84b217729f
Manas Acharyya <manas.acharyya.nits.cse18@gmail.com>
2021-04-16 18:03:34 +0530

    Added new TriedItOnce Carousel

--- portfolio/src/components/trieditonce/trieditonce.js
+++ portfolio/src/components/trieditonce/trieditonce.js
@@ -1,91 +1,115 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import Carousel from 'react-bootstrap/Carousel'
-import Container from 'react-bootstrap/Container'
+import Carousel from 'react-multi-carousel';
+import "react-multi-carousel/lib/styles.css";
 
 import './trieditonce.css'
 
 export function TriedItOnce() {
     return (
         <div className="TriedItOnce">
-            <h1 style={{ fontFamily: 'Quicksand' }}>Tried It Once !!!</h1>
-            <Carousel fade interval='3000' pause='hover' wrap='true'>
-                <Carousel.Item >
-                    <Container>
-                        <Row>
-                            <Col>
-                                <img
-                                    className="d-inline"
-                                    src="./assets/terraform.png"
-                                    alt="First slide"
-                                    height="60rem"
-                                />
-                            </Col>
-                            <Col>
-                                <img
-                                    className="d-inline"
-                                    src="./assets/figma.png"
-                                    alt="Second slide"
-                                    height="60rem"
-                                />
-                            </Col>
-                            <Col>
-                                <img
-                                    className="d-inline"
-                                    src="./assets/application-insights.png"
-                                    alt="Second slide"
-                                    height="60rem"
-                                />
-                            </Col>
-                            <Col>
-                                <img
-                                    className="d-block"
-                                    src="./assets/elastic-bean-stalk.png"
-                                    alt="Third slide"
-                                    height="60rem"
-                                />
-                            </Col>
-                        </Row>
-                    </Container>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <Container>
-                        <Row>
-                            <Col>
-                                <img
-                                    className="d-block"
-                                    src="./assets/php.svg"
-                                    alt="Third slide"
-                                    height="60rem"
-                                />
-                            </Col>
-                            <Col>
-                                <img
-                                    className="d-block"
-                                    src="./assets/spark.png"
-                                    alt="Third slide"
-                                    height="60rem"
-                                />
-                            </Col>
-                            <Col>
-                                <img
-                                    className="d-block"
-                                    src="./assets/superset.png"
-                                    alt="Third slide"
-                                    height="60rem"
-                                />
-                            </Col>
-                            <Col>
-                                <img
-                                    className="d-block"
-                                    src="./assets/fhir.png"
-                                    alt="Third slide"
-                                    height="60rem"
-                                />
-                            </Col>
-                        </Row>
-                    </Container>
-                </Carousel.Item>
+            <h1 style={{ fontFamily: 'Quicksand', marginBottom: '2rem' }}>Tried It Once !!!</h1>
+            <Carousel
+                additionalTransfrom={0}
+                arrows
+                autoPlaySpeed={3000}
+                centerMode={true}
+                className=""
+                containerClass="container-with-dots"
+                dotListClass=""
+                draggable
+                focusOnSelect={false}
+                infinite
+                itemClass=""
+                keyBoardControl
+                minimumTouchDrag={80}
+                renderButtonGroupOutside={false}
+                renderDotsOutside={false}
+                responsive={{
+                    desktop: {
+                        breakpoint: {
+                            max: 3000,
+                            min: 1024
+                        },
+                        items: 3,
+                        partialVisibilityGutter: 40
+                    },
+                    mobile: {
+                        breakpoint: {
+                            max: 464,
+                            min: 0
+                        },
+                        items: 1,
+                        partialVisibilityGutter: 30
+                    },
+                    tablet: {
+                        breakpoint: {
+                            max: 1024,
+                            min: 464
+                        },
+                        items: 2,
+                        partialVisibilityGutter: 30
+                    }
+                }}
+                showDots={false}
+                sliderClass=""
+                slidesToSlide={1}
+                swipeable
+            >
+                <div>
+                    <img
+                        src="./assets/terraform.png"
+                        alt="Terraform"
+                        height="60rem"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="./assets/figma.png"
+                        alt="figma"
+                        height="60rem" />
+                </div>
+                <div>
+                    <img
+                        src="./assets/application-insights.png"
+                        alt="Azure Application Insights"
+                        height="60rem"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="./assets/elastic-bean-stalk.png"
+                        alt="AWS Elastic Beanstalk"
+                        height="60rem"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="./assets/php.svg"
+                        alt="PHP"
+                        height="60rem"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="./assets/spark.png"
+                        alt="Apache Spark"
+                        height="60rem"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="./assets/superset.png"
+                        alt="Apache Superset"
+                        height="60rem"
+                    />
+                </div>
+                <div>
+                    <img
+                        src="./assets/fhir.png"
+                        alt="FHIR"
+                        height="60rem"
+                    />
+                </div>
             </Carousel>
         </div>
     )
